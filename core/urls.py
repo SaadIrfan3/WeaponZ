b079ea0bf7638959ff8f2ecf7c4abc81c1ff6ffe
saad <saadirfan173@gmail.com>
2025-08-05 13:44:45 +0500

    main page design

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/mainpage/',views.main_page,name = 'mainpa'),
+    path('api/signup/',views.signup_page,name = 'signup'),
+    path('api/login/',views.login_page,name = 'login'),
+    path('api/shop/',views.shop_weapons,name = 'shop'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+@csrf_exempt
+def main_page(request):
+    return render(request,'core/mainpage.html')
+
+@csrf_exempt
+def signup_page(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username') or request.GET.get('username')
+        email = request.POST.get('email') or request.GET.get('email')
+        password = request.Post.get('password') or request.Get.get('password')
+
+        if not username or not email or not password:
+            return JsonResponse({'message': 'PLez enter all credentials'})
+        if User.objects.filter.get(username=username).exists() or User.objects.filter.get(email=email):
+            return JsonResponse({'message': 'username or email already exists'})
+        User.objects.create_user(
+        username=username,
+        email=email,
+        password=password)
+
+        return redirect('login')
+    return render(request,'core/signup.html')
+
+@csrf_exempt
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username') or request.GET.get('username')
+        email = request.POST.get('email') or request.GET.get('email')
+        password = request.POST.get('password') or request.POST.get('password')
+
+        if not username or not email or not password:
+            return JsonResponse({'message': 'Please enter the correct credentials to login'})
+        user = authenticate(username=username,email=email,password=password)
+        if user is not None:
+            login(request,user)
+            return (request,'core/shop.html')
+        else:
+            return JsonResponse({'error': 'invalid credentials bro'})
+    return render(request,'core/login.html')
+
+@csrf_exempt
+def shop_weapons(request):
+    return render(request,'core/shop.html')
+
